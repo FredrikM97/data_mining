dc0f8aab122f0c4f1fe3eb6475972fe4094b7cf0
FredrikM97 <fredrik9779@gmail.com>
2020-10-18 19:29:37 +0200

    Updates

--- Jupyter Notebook.bat
+++ Jupyter Notebook.bat
@@ -2,4 +2,4 @@ CD C:\
 CALL TITLE Jupyter Notebook
 SET drivePath=%~dp0
 SET drivePath=%drivePath:\=^/%
-CALL jupyter notebook --notebook-dir "%drivePath%"
+CALL jupyter notebook --notebook-dir "%drivePath%" --ip 192.168.1.46 --port 8888
